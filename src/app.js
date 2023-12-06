99a83e1fb18163ab9bfdd8628ff53ea826c6f859
Royce119 <madcoder911@gmail.com>
2023-12-06 15:53:09 +0200

    fixed cors

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const productsRouter = require("./routes/products/products.router");
 const cors = require("cors");
 const emailRouter = require("./routes/email/email.router");
-// app.use(
-//     cors({
-//       origin: [
-//         "http://localhost:3000",
-//         "https://booking-mern.vercel.app/",
-//         "https://booking-mern.vercel.app",
-//         "http://localhost:3001",
-//       ],
-
-//       credentials: true, //i
-//     })
-//   );
 const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://booking-mern.vercel.app/",
+      "https://booking-mern.vercel.app",
+      "http://localhost:3001",
+    ],
+
+    credentials: true, //i
+  })
+);
 app.use(express.json());
 app.use(emailRouter);
 app.use(productsRouter);
