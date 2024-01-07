ec67ab7b7ad1ca44e45ce367bbf2a4b8c4c9fb49
Royce119 <madcoder911@gmail.com>
2024-01-07 20:53:36 +0200

    fixing cookies not being set

--- src/routes/auth/auth.controller.js
+++ src/routes/auth/auth.controller.js
@@ -45,7 +45,7 @@ const login = async (req, res, next) => {
     //Set cookie
     res.cookie("access_token", token, {
       httpOnly: true,
-      secure: true,
+      // secure: true,
       sameSite: "None",
     });
     res.status(200).json({ ...otherDetails });
