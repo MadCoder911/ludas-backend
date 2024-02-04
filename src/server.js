ea545badfb6f416bb7c64949509eb0916b55d98f
Royce119 <madcoder911@gmail.com>
2024-02-04 23:34:20 +0200

    changed port

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 require("dotenv").config();
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 const startServer = async () => {
   await mongoConnect();
   app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT} !`);
   });
 };
 startServer();
